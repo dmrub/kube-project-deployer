8c575a69f4744e1c095cfd9abe02dbca1fab4440
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2020-08-14 18:42:26 +0200

    Move template preprocessor to the shlib.sh script

--- scripts/shlib.sh
+++ scripts/shlib.sh
@@ -413,3 +413,111 @@ EOF
 match-pod() {
     wait-for-pod --run-checks 1 --trials 1 --delay 0 "$@"
 }
+
+# Template Preprocessor
+
+define-template-preprocessor-funcs() {
+
+    : "${TEMPLATE_OVERWRITE:=true}"
+
+    preprocess-filename() {
+        # By default don't change file name
+        echo "$1"
+    }
+
+    action-preprocess() {
+        local dest_file
+        case "$SRC_FILE" in
+            *.mustache)
+                # remove mustache
+                dest_file=${SRC_FILE%.*}
+                # preprocess filename
+                dest_file=$(preprocess-filename "$dest_file")
+                echo "In $SRC_FILE_DIR: preprocess $FILE_BN -> $DEST_DIR/$dest_file"
+                if [[ -e "$DEST_DIR/$dest_file" ]] && ! is-true "$TEMPLATE_OVERWRITE"; then
+                    fatal "File $DEST_DIR/$dest_file already exists and overwriting is disabled, abort the action"
+                fi
+                mo <"$SRC_FILE" >"$DEST_DIR/$dest_file"
+                ;;
+            *)
+                fatal "Unknown file extension $FILE_EXT in $SRC_FILE"
+                ;;
+        esac
+    }
+
+    action-copy-file() {
+        local dest_file
+        dest_file=$SRC_FILE
+        dest_file=$(preprocess-filename "$dest_file")
+        echo "In $SRC_FILE_DIR: copy $FILE_BN -> $DEST_DIR/$dest_file"
+        if [[ -e "$DEST_DIR/$dest_file" ]] && ! is-true "$TEMPLATE_OVERWRITE"; then
+            fatal "File $DEST_DIR/$dest_file already exists and overwriting is disabled, abort the action"
+        fi
+        cp -a "$SRC_FILE" "$DEST_DIR/$dest_file"
+    }
+
+    preprocess-templates() {
+        if [[ -z "$TEMPLATE_DEST_DIR" ]]; then
+            fatal "TEMPLATE_DEST_DIR variable is not set"
+        fi
+
+        dest_dir=$(abspath "$TEMPLATE_DEST_DIR")
+        echo "Create template destination directory $dest_dir"
+        mkdir -p "$dest_dir"
+
+        echo "Preprocessing templates in $TEMPLATE_SRC_DIR ..."
+        cd "$TEMPLATE_SRC_DIR"
+        while IFS= read -r -d '' file; do
+            # Ignore source directory
+            if [[ "$file" = "." ]]; then
+                continue
+            fi
+
+            # Create directory
+            if [[ -d "$file" ]]; then
+                if [[ ! -d "$dest_dir/$file" ]]; then
+                    echo "Create directory $dest_dir/$file"
+                    mkdir -p "$dest_dir/$file"
+                fi
+                continue
+            fi
+
+            file_action=
+            case "$file" in
+                *.mustache) file_action="action-preprocess" ;;
+                *) file_action="action-copy-file" ;;
+            esac
+
+            file_to_match=${file#"."}
+            for ((i = 0; i < ${#TEMPLATE_FILE_ACTIONS[@]}; i += 2)); do
+                pat=${TEMPLATE_FILE_ACTIONS[$i]}
+                action=${TEMPLATE_FILE_ACTIONS[$i + 1]}
+                if [[ -n "$pat" ]]; then
+                    # shellcheck disable=SC2053
+                    if [[ "$file_to_match" = $pat ]]; then
+                        case "$action" in
+                            ignore) file_action="" ;;
+                            copy) file_action="action-copy-file" ;;
+                            preprocess) file_action="action-preprocess" ;;
+                            *) fatal "Unknown file action $action in TEMPLATE_FILE_ACTIONS variable" ;;
+                        esac
+                        break
+                    fi
+                fi
+            done
+
+            SRC_FILE=$file
+            DEST_DIR=$dest_dir
+            SRC_FILE_DIR=$( (cd "$(dirname -- "$SRC_FILE")" && pwd -P))
+            FILE_BN=$(basename -- "$SRC_FILE")
+            FILE_EXT=${FILE_BN##*.}
+            # shellcheck disable=SC2034
+            FILE_FN="${FILE_BN%.*}"
+
+            if [[ -n "$file_action" ]]; then
+                "$file_action"
+            fi
+
+        done < <(find "." -print0)
+    }
+}
